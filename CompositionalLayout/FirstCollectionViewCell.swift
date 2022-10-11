11496be9113102d30fba28331b15dac551244469
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2022-10-11 21:12:41 +0300

    Assets added

--- CompositionalLayout/FirstCollectionViewCell.swift
+++ CompositionalLayout/FirstCollectionViewCell.swift
@@ -11,6 +11,25 @@ class FirstCollectionViewCell: UICollectionViewCell {
     
     static let identifier   = "FirstCollectionViewCell"
     
+    private let text: UILabel   = {
+        
+        let text    = UILabel()
+        
+        text.translatesAutoresizingMaskIntoConstraints = false
+        
+        text.text   = "FirstCell"
+        
+        return text
+        
+    }()
+    
+    private func applyConstraints(){
+        NSLayoutConstraint.activate([
+            text.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            text.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
     required init?(coder: NSCoder) {
         fatalError()
     }
@@ -19,6 +38,8 @@ class FirstCollectionViewCell: UICollectionViewCell {
         super.init(frame: frame)
         
         contentView.backgroundColor     = .systemRed
+        contentView.addSubview(text)
+        applyConstraints()
         
     }
     
