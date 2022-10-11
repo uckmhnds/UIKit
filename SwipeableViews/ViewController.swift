1b4a405145bd161a9416b41eca2b1105c1936a67
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2022-10-11 22:18:43 +0300

    Swipeable Views with bottom control

--- SwipeableViews/ViewController.swift
+++ SwipeableViews/ViewController.swift
@@ -26,6 +26,68 @@ class ViewController: UIViewController {
         return view
     }()
     
+    private let prevButton: UIButton = {
+        let button      = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints    = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        button.addTarget(nil, action: #selector(prevButtonEvent), for: .touchUpInside)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button      = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints    = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.systemPink, for: .normal)
+        button.addTarget(nil, action: #selector(nextButtonEvent), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var pageControl : UIPageControl = {
+        let pc                              = UIPageControl()
+        pc.numberOfPages                    = self.numberOfPages
+        pc.currentPage                      = 0
+        pc.currentPageIndicatorTintColor    = .systemPink
+        pc.pageIndicatorTintColor           = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+        return pc
+    }()
+    
+    fileprivate func setBottomControls() {
+        
+        let bottomControlStackView  = UIStackView(arrangedSubviews: [self.prevButton, self.pageControl, self.nextButton])
+        
+        bottomControlStackView.translatesAutoresizingMaskIntoConstraints    = false
+        bottomControlStackView.distribution = .fillEqually
+        
+        self.view.addSubview(bottomControlStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomControlStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlStackView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc private func nextButtonEvent() {
+        
+        let nextIndex = min(self.pageControl.currentPage + 1, self.numberOfPages - 1)
+        let indexPath = IndexPath(item: nextIndex, section: 0)
+        self.pageControl.currentPage = nextIndex
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    @objc private func prevButtonEvent() {
+        
+        let nextIndex = max(self.pageControl.currentPage - 1, 0)
+        let indexPath = IndexPath(item: nextIndex, section: 0)
+        self.pageControl.currentPage = nextIndex
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,6 +99,8 @@ class ViewController: UIViewController {
         
         collectionView.delegate         = self
         collectionView.dataSource       = self
+        
+        setBottomControls()
         
     }
     
@@ -60,7 +124,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
         
         let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
         
-        cell.backgroundColor    = indexPath.item % 2 == 0 ? .systemRed : .systemYellow
+        cell.backgroundColor    = indexPath.item % 2 == 0 ? .systemGray3 : .systemGray4
         
         return cell
     }
@@ -72,8 +136,18 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: view.frame.height)
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return marginBetweenCell
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        
+        let scrollOffsetX   = targetContentOffset.pointee.x
+        let frameWidth      = view.frame.width
+        
+        pageControl.currentPage = Int(scrollOffsetX / frameWidth)
+        
     }
     
 }
