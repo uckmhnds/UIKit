e9765bd15067d235510233e95eaa9c97077936cc
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2022-10-11 12:10:21 +0300

    Initial Commit

---
+++ SwipeableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeableViews
+//
+//  Created by Abdurrahman Gazi Yavuz on 11.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
