25874657dc32bc025ad86cf47548355ed9824634
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2022-11-02 16:12:56 +0300

    Initial Commit

---
+++ ModalPresentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModalPresentation
+//
+//  Created by Abdurrahman Gazi Yavuz on 11/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
