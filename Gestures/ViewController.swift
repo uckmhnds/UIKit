59a26eef1735cf2c763142f036d53d34eb8d9096
Abdurrahman Gazi Yavuz <uckmhnds@hotmail.com>
2022-11-14 20:29:46 +0300

    Initial Commit

---
+++ Gestures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gestures
+//
+//  Created by Abdurrahman Gazi Yavuz on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
